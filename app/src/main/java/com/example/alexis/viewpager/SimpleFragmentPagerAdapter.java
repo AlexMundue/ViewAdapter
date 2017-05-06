3d88c4a0e47843a6da9722524584cce5bd3c10e8
Alexis Muñoz <alexismun53@gmail.com>
2017-05-06 12:15:51 -0500

    1.0

---
+++ app/src/main/java/com/example/alexis/viewpager/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.alexis.viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by alexi on 06/05/2017.
+ */
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position == 0){
+            return new MondayFragment();
+        }else if( position == 1){
+            return new TuesdayFragment();
+        }else {
+            return new WednesdayFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
---
+++ app/src/main/java/com/example/alexis/viewpager/TuesdayFragment.java
@@ -0,0 +1,21 @@
+package com.example.alexis.viewpager;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alexi on 06/05/2017.
+ */
+
+public class TuesdayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tuesday, container, false);
+    }
+
+}
